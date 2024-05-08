daa5f036b56d987e15365249dbf1988c96b334e4
littleviil <netlena30@gmail.com>
2024-05-08 04:36:00 +0300

    *

--- render.js
+++ render.js
@@ -1,12 +1,12 @@
 import { cardElement } from './dom.js';
 import { addLikeClickButton, editComments } from './events.js';
-import { buttonElement, inputName, inputText } from './dom.js';
+import { buttonElement, inputName, inputText, loadingComment } from './dom.js';
 import { postAPI } from './API.js';
 import { checkInputForm, checkDeleteButton } from './check.js';
 
 const render = (comments) => {
-    const userHtml = comments.map((user, index) => {
-        return `<li class="comment" id="comment-block" data-index="${index}">
+  const userHtml = comments.map((user, index) => {
+    return `<li class="comment" id="comment-block" data-index="${index}">
         <div class="comment-header" data-index="${index}">
           <div>${user.author}</div>
           <div>${user.date}</div>
@@ -24,50 +24,53 @@ const render = (comments) => {
           <button class="add-form-button edit-button" data-index="${index}">Редактировать</button>
         </div>
       </li>`
-    }).join('');
-    cardElement.innerHTML = userHtml;
+  }).join('');
+  cardElement.innerHTML = userHtml;
 
-    addLikeClickButton(comments);
-    checkInputForm();
-    checkDeleteButton(comments);
-    editComments(comments);
+  addLikeClickButton(comments);
+  checkInputForm();
+  checkDeleteButton(comments);
+  editComments(comments);
 };
 
 export { render };
 
 const initClickHandler = (comments) => {
-    //Ввод
-    buttonElement.addEventListener("click", (event) => {
-        event.stopPropagation();
-        inputText.classList.remove("error");
-        inputName.classList.remove("error");
+  //Ввод
+  buttonElement.addEventListener("click", (event) => {
+    event.stopPropagation();
+    inputText.classList.remove("error");
+    inputName.classList.remove("error");
 
-        if ((inputText.value.length === 0) || (inputName.value.length === 0)) {
-            if ((inputText.value.length === 0) && (inputName.value.length === 0)) {
-                inputName.classList.add("error");
-                inputText.classList.add("error");
-                return;
-            }
-            else if (inputName.value.length === 0) {
-                inputName.classList.add("error");
-                return;
-            }
-            else if (inputText.value.length === 0) {
-                inputText.classList.add("error");
-                return;
-            }
-        };
-        loadingComment.classList.remove('load');
-        document.getElementById('form').classList.add('load');
+    if ((inputText.value.length === 0) || (inputName.value.length === 0)) {
+      if ((inputText.value.length === 0) && (inputName.value.length === 0)) {
+        inputName.classList.add("error");
+        inputText.classList.add("error");
+        return;
+      }
+      else if (inputName.value.length === 0) {
+        inputName.classList.add("error");
+        return;
+      }
+      else if (inputText.value.length === 0) {
+        inputText.classList.add("error");
+        return;
+      }
+    };
+    loadingComment.classList.remove('load');
+    document.getElementById('form').classList.add('load');
 
-        postAPI(inputName, inputText);
+    postAPI(inputName, inputText);
 
-        loadingComment.classList.add('load');
-        document.getElementById('form').classList.remove('load');
+    inputName.value = "";
+    inputText.value = "";
+    
+    loadingComment.classList.add('load');
+    document.getElementById('form').classList.remove('load');
 
-        render(comments);
-        checkInputForm();
-    });
+    render(comments);
+    checkInputForm();
+  });
 };
 
 export { initClickHandler };

a7da1d4207791d0a4d49906a6cb5720f28df0bb6
littleviil <netlena30@gmail.com>
2024-06-22 08:23:46 +0300

    package.json + date-fns + .gitignore

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // import { initClickHandler } from './render.js';
 import { loadingForm } from './dom.js';
 import { getAPI, comments } from './API.js';
+import { format, addDays } from 'date-fns';
 
 export let user = null;
 export function setUser(value) {
